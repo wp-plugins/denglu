9abf309d514ec123739a9bb4d4383841ef2e2c54
smyx <smyx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-24 09:56:39 +0000

    git-svn-id: https://plugins.svn.wordpress.org/denglu/tags/1.5@535371 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,87 @@
+﻿=== Plugin Name ===
+Contributors: smyx
+Donate link: http://www.denglu.cc/source/denglu-comments.html
+Tags: 灯鹭,社会化评论,Denglu,Denglu评论,评论,Denglu Comment,disqus,pinglunla,uyan,youyan,duoshuo,widget,sina,tencent,qq,qzone,Share
+Requires at least: 3.0
+Tested up to: 3.3.2
+Stable tag: 1.5
+
+灯鹭社会化评论产品。
+
+== Description ==
+
+[演示页面](http://www.denglu.cc/demo.html) 
+
+[帮助中心](http://bbs.denglu.cc/forum-27-1.html) 
+
+[WordPress连接微博](http://wordpress.org/extend/plugins/wp-connect/) —— 集成了Denglu评论（还包括同步文章到微博、合作网站帐号登录WordPress等实用功能）
+
+= 详细描述 =
+
+1、评论数据会保存一份在WordPress本地数据库，不必担心评论丢失。（非实时同步，5分钟一次）
+
+2、灯鹭控制台"评论管理"页面的评论状态（待审核、垃圾评论、回收站、永久删除）也会同步到本地数据库。（非实时同步，5分钟一次）
+
+3、评论支持SE0。
+
+4、支持网站原有评论导入到灯鹭云端服务器，并在社会化评论框显示。从灯鹭服务器导到本地的评论被回复了，可以再把这条回复导入到灯鹭服务器。
+
+5、支持调用最新评论（小工具）
+
+= 产品介绍 =
+
+1、完善的评论框及评论管理、评论自定义设置，减少您的开发成本。
+
+2、提供丰富的数据分析功能，让你随时掌握网站动态。
+
+3、精准的垃圾评论过滤，展示优质的评论内容，提高网站的整体质量。
+
+4、跟微博、SNS紧密结合，集成登录、评论、一键分享，用户同步的评论可以抓取回来显示在您的网站上，为网站带来人气、回流。
+
+5、支持自定义评论模板，方便您根据自身的网站风格设计漂亮的评论界面。
+
+== Installation ==
+
+1. Upload the `denglu-comment` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Configure the settings using "WP Connect" under Settings panel.
+
+下载 社会化评论 插件，上传denglu-comment目录及其文件到 "/wp-content/plugins/" 插件目录，在后台管理中激活插件，到设置页面开启功能并设置等.
+
+== Screenshots ==
+
+1. 登录社会化评论 后台界面
+
+== Changelog ==
+
+= 1.5.0 =
+
+新增：从灯鹭服务器导到本地的评论被回复了，可以再把这条回复导入到灯鹭服务器。
+
+= 1.4.0 =
+*2012/04/09
+
+新增：评论数据会保存一份在WordPress本地数据库，不必担心评论丢失。（非实时同步，5分钟一次）
+
+新增：灯鹭控制台"评论管理"页面的评论状态（待审核、垃圾评论、回收站、永久删除）也会同步到本地数据库。（非实时同步，5分钟一次）
+
+新增：评论支持SE0。
+
+删除：灯鹭评论数调用，因为加入评论数据同步，不再需要浪费网络资源，直接调用本地评论数即可。
+
+= 1.3.0 =
+*2012/03/05
+优化：文章评论数和最新评论的获取方式，显示速度更快。
+
+= 1.2.0 =
+*2012/03/01
+新增文章评论数和最新评论（小工具拖拽）功能。
+
+= 1.1.0 =
+*2012/02/22
+增加评论导入功能。
+
+= 1.0.0 =
+*2012/01/16
+初始版本
+
---
+++ denglu.php
@@ -0,0 +1,135 @@
+<?php
+/*
+Plugin Name: Denglu评论
+Author: 水脉烟香
+Author URI: http://www.smyx.net/
+Plugin URI: http://wordpress.org/extend/plugins/denglu/
+Description: 灯鹭网开发的社会化评论 —— Denglu评论 ( <a href="http://www.denglu.cc/" target="_blank">灯鹭网</a> 版权所有。)
+Version: 1.5
+*/
+
+$wptm_basic = get_option('wptm_basic');
+$wptm_comment = get_option('wptm_comment');
+
+add_action('admin_notices', 'denglu_comments_warning');
+function denglu_comments_warning() {
+	if (version_compare(WP_CONNECT_VERSION, '2.0', '>')) {
+		echo '<div class="updated">';
+		echo "<p><strong>检测到您正在使用“WordPress连接微博”插件，请直接使用“WordPress连接微博”的评论设置功能，无需另外安装 灯鹭社会化评论 插件，谢谢您的支持！</strong></p>";
+		echo '</div>';
+	}
+}
+
+add_action('admin_menu', 'denglu_comments_add_page');
+function denglu_comments_add_page() {
+	if (version_compare(WP_CONNECT_VERSION, '2.0', '<')) {
+		add_options_page('Denglu评论', 'Denglu评论', 'manage_options', 'denglu', 'denglu_comments_do_page');
+	}
+}
+
+if (!function_exists('default_values')) { // 设置默认值
+	function default_values($key, $vaule, $array) {
+		if (!is_array($array)) {
+			return true;
+		} else {
+			if ($array[$key] == $vaule || !array_key_exists($key, $array)) {
+				return true;
+		    }
+		}
+	} 
+}
+
+if (!function_exists('denglu_comments') && $wptm_comment['enable_comment'] && $wptm_basic['appid'] && $wptm_basic['appkey']) {
+	include_once(dirname(__FILE__) . '/denglu.func.php'); // 灯鹭自定义函数
+	add_filter('comments_template', 'denglu_comments');
+	function denglu_comments($file) {
+		global $post;
+		return dirname(__FILE__) . '/comments.php';
+	} 
+}
+
+// 设置
+function denglu_comments_do_page() {
+	if (isset($_POST['basic_options'])) {
+		update_option("wptm_basic", array('appid'=>trim($_POST['appid']), 'appkey'=>trim($_POST['appkey'])));
+	} elseif (isset($_POST['comment_options'])) {
+		update_option("wptm_comment", array('enable_comment' => trim($_POST['enable_comment']), 'manual' => trim($_POST['manual']), 'comments_open' => trim($_POST['comments_open']), 'dcToLocal' => trim($_POST['dcToLocal']), 'latest_comments' => trim($_POST['latest_comments']), 'enable_seo' => trim($_POST['enable_seo'])));
+	} elseif (isset($_POST['comment_delete'])) {
+		delete_option("wptm_basic");
+		delete_option("wptm_comment");
+	} elseif (isset($_POST['importComment'])) { // 评论导入到灯鹭
+		if (function_exists('denglu_importComment')) {
+			denglu_importComment();
+			echo '<div class="updated"><p><strong>评论导入成功！</strong></p></div>';
+		} else {
+			echo '<div class="updated"><p><strong>请先开启社会化评论，并填写APP ID和APP Key</strong></p></div>';
+		}
+	}
+	$wptm_basic = get_option('wptm_basic');
+	$wptm_comment = get_option('wptm_comment');
+?>
+<div class="wrap">
+  <h2>Denglu评论</h2>
+      <p style="color:green"><strong>使用前，请先在 <a href="http://open.denglu.cc" target="_blank">灯鹭控制台</a> 注册帐号，并创建站点，之后在下面填写APP ID 和 APP Key ，评论的相关设置及管理，请在灯鹭控制台操作。<br />如果您还需要使用合作网站登录及同步功能，请直接下载 <a href="http://www.denglu.cc/source/wordpress2.0.html" target="_blank">WordPress连接微博</a> V2插件（集成了社会化评论），谢谢您的支持！</strong></p>
+      <form method="post" action="">
+        <?php wp_nonce_field('basic-options');?>
+        <h3>站点设置</h3>
+	    <table class="form-table">
+		    <tr>
+			    <td width="25%" valign="top">APP ID: </td>
+			    <td><label><input type="text" name="appid" size="32" value="<?php echo $wptm_basic['appid'];?>" /></label> (必填)</td>
+		    </tr>
+		    <tr>
+			    <td width="25%" valign="top">APP Key: </td>
+			    <td><label><input type="text" name="appkey" size="32" value="<?php echo $wptm_basic['appkey'];?>" /></label> (必填)</td>
+		    </tr>
+        </table>
+        <p class="submit">
+          <input type="submit" name="basic_options" class="button-primary" value="<?php _e('Save Changes') ?>" />
+        </p>
+      </form>
+      <form method="post" action="">
+        <?php wp_nonce_field('comment-options');?>
+        <h3>评论设置</h3>
+	    <table class="form-table">
+            <tr>
+                <td width="25%" valign="top">是否开启“社会化评论”功能</td>
+                <td><input name="enable_comment" type="checkbox" value="1" <?php if($wptm_comment['enable_comment']) echo "checked "; ?>></td>
+            </tr>
+		    <tr>
+			    <td width="25%" valign="top">自定义函数</td>
+			    <td><label><input name="manual" type="checkbox" value="1" <?php if($wptm_comment['manual']) echo "checked "; ?> />自己在主题添加函数（不推荐使用）</label><code>&lt;?php dengluComments();?&gt;</code></td>
+		    </tr>
+		    <tr>
+			    <td width="25%" valign="top">单篇文章评论开关</td>
+			    <td><label><input name="comments_open" type="checkbox" value="1" <?php if(default_values('comments_open', 1, $wptm_comment)) echo "checked ";?> />继承WordPress已有的评论开关，即当某篇文章关闭评论时，也不使用社会化评论功能。</label></td>
+		    </tr>
+		    <tr>
+			    <td width="25%" valign="top">同步评论到本地</td>
+			    <td><label><input name="dcToLocal" type="checkbox" value="1" <?php if(default_values('dcToLocal', 1, $wptm_comment)) echo "checked ";?> />灯鹭评论内容保存一份在WordPress本地评论数据库（非实时同步，5分钟一次）</label></td>
+		    </tr>
+		    <tr>
+			    <td width="25%" valign="top">最新评论</td>
+			    <td><label><input name="latest_comments" type="checkbox" value="1" <?php if($wptm_comment['latest_comments']) echo "checked ";?> />是否开启侧边栏“最新评论”功能 (开启后到<a href="widgets.php">小工具</a>拖拽激活)</label></td>
+		    </tr>
+		    <tr>
+			    <td width="25%" valign="top">SEO支持</td>
+			    <td><label><input name="enable_seo" type="checkbox" value="1" <?php if($wptm_comment['enable_seo']) echo "checked "; ?> />评论支持SEO，让搜索引擎能爬到评论数据</label></td>
+		    </tr>
+        </table>
+        <p class="submit">
+          <input type="submit" name="comment_options" class="button-primary" value="<?php _e('Save Changes') ?>" />
+        </p>
+      </form>
+	  <h3>导入导出</h3>
+	  <p>导入数据到灯鹭平台。导入后，您原有的网站评论将在“Denglu评论”的评论框内显示。</p>
+	  <p><form method="post" action=""><span class="submit"><input type="submit" name="importComment" value="评论导入" /> (可能需要一些时间，请耐心等待！)</span></form></p>
+	  <h3>卸载插件</h3>
+	  <p>假如您要使用“WordPress连接微博” V2.x插件，可以不必卸载本插件，现有的设置不变！</p>
+      <form method="post" action="">
+	    <?php wp_nonce_field('comment-delete');?>
+		<span class="submit"><input type="submit" name="comment_delete" value="卸载插件" onclick="return confirm('您确定要卸载社会化评论？')" /></span>
+	  </form>
+</div>
+<?php
+}
